5e6a23fa2f49d66d6333a3de54c42bfcdc138cae
Asim Abdalla <asimabdalla99@gmail.com>
2025-05-13 23:34:43 +0200

    fix: update WORK_DIR path and uncomment language options in arc-easy.yaml

--- prompt_with_API/scripts/run.sh
+++ prompt_with_API/scripts/run.sh
@@ -2,24 +2,24 @@ set -e # Exit immediately if a command exits with a non-zero status.
 set -u # Treat unset variables as an error when substituting.
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-WORK_DIR=$SCRIPT_DIR/../..
+WORK_DIR=$SCRIPT_DIR/..
 TASK_DIR=$WORK_DIR/tasks
 
 TASKS=(
     "arc-easy"
-    "afrimmlu"
-    "afrimgsm"
-    "afrisenti"
-    "afrihate"
-    "afrixnli"
-    "openai_mmlu"
-    "afriqa"
-    "belebele"
-    "masakhanews"
-    "masakhapos"
-    "masakhaner"
-    "intent"
-    "xlsum"
+    # "afrimmlu"
+    # "afrimgsm"
+    # "afrisenti"
+    # "afrihate"
+    # "afrixnli"
+    # "openai_mmlu"
+    # "afriqa"
+    # "belebele"
+    # "masakhanews"
+    # "masakhapos"
+    # "masakhaner"
+    # "intent"
+    # "xlsum"
 )
 
 MODEL_NAMES=(
@@ -38,7 +38,7 @@ for i in "${!MODEL_NAMES[@]}"; do
     echo "Running task with model: $MODEL_NAME"
     for TASK in "${TASKS[@]}"; do
         echo "Running task: $TASK"
-        OUTPUT_DIR=$WORK_DIR/results/${TASK}/$MODEL_ABBR
+        OUTPUT_DIR=$WORK_DIR/../results/${TASK}/$MODEL_ABBR
         python3 $WORK_DIR/run.py \
                 --tasks $TASK_DIR/$TASK.yaml \
                 --model $MODEL_NAME \
